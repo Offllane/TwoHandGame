9bf6038c0e49638dd1a1355e7c226c853fe1a79b
offllane <offllane@mail.ru>
2020-06-29 22:42:56 +0300

    add help menu

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,22 @@
 const POPUP = document.getElementsByClassName("popup")[0];
 
-function makePopupVisible()
-{
+function makePopupVisible() {
   POPUP.style.display = "flex";
   document.getElementsByClassName("popup-score")[0].innerHTML = `Your score is: ${points}`;
+}
+
+
+$(document).ready(function()
+{
+  PopUpHide();
+});
+
+function PopUpShow()
+{
+  $("#popup1").show();
+}
+
+function PopUpHide()
+{
+  $("#popup1").hide();
+}
