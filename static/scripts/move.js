82d6e9675b1fb27fc8a8f10ce7dd75412154cd66
offllane <offllane@mail.ru>
2020-07-10 18:25:22 +0300

    remade script to readable view. Add comments

--- static/scripts/move.js
+++ static/scripts/move.js
@@ -1,3 +1,13 @@
+/**
+ * @namespace MovementFunctions
+ */
+
+/**
+ * Remove all hero pictures from field
+ * @memberof MovementFunctions
+ * @param {Element} heroPosition - current hero position
+ */
+
 function clearMovement(heroPosition)
 {
   heroPosition.classList.remove("firstHero_back-side");
@@ -10,98 +20,144 @@ function clearMovement(heroPosition)
   heroPosition.classList.remove("secondHero");
 }
 
-function moveHero(heroCoordinates, heroName, direction)
+
+/**
+ * Check hero position and make heroSpeed 0 if hero want to out from game area
+ * @memberof MovementFunctions
+ * @param {array} heroCoordinates - two hero coordinates
+ * @param {string} direction - movement direction
+ * @returns {int} heroSpeed
+ */
+function checkBorder(heroCoordinates, direction)
 {
   let heroSpeed = config.heroSpeed;
-  let currentHeroStep = document.querySelector(
-    '[posX = "' + heroCoordinates[0] + '"][posY = "' + heroCoordinates[1] + '"]'
-  );
-  clearMovement(currentHeroStep);
-  let heroPosition;
   if (heroCoordinates[0] == 1 && direction == "left")
   {
     heroSpeed = 0;
   }
   if (heroCoordinates[0] == 30 && direction == "right")
   {
-     heroSpeed = 0;
+    heroSpeed = 0;
   }
   if (heroCoordinates[1] == 1 && direction == "up")
   {
-     heroSpeed = 0;
+    heroSpeed = 0;
   }
   if (heroCoordinates[1] == 30 && direction == "down")
   {
-     heroSpeed = 0;
+    heroSpeed = 0;
   }
+  return heroSpeed;
+}
 
-  clearAttackZone();
+
+/**
+ * Show first hero turned in the correct direction
+ * @memberof MovementFunctions
+ * @param {string} direction - last movement side
+ */
+function rotateFirstHero(direction, heroPosition)
+{
   switch (direction)
   {
     case "up":
-      heroCoordinates[1] -= heroSpeed;      
+      heroPosition.classList.add("firstHero_back-side");
       break;
     case "down":
-      heroCoordinates[1] +=  heroSpeed;
+      heroPosition.classList.remove("firstHero_back-side");
       break;
     case "left":
-      heroCoordinates[0] -=  heroSpeed;
+      heroPosition.classList.add("firstHero_left-side");
       break;
     case "right":
-      heroCoordinates[0] +=  heroSpeed;
-      break;
-    default:
-      break;
-  }
-  heroPosition = document.querySelector('[posX = "' + heroCoordinates[0] + '"][posY = "' + heroCoordinates[1] + '"]');
-  createNewAttackZone();
-  heroPosition.classList.add(heroName);
-
-  switch (heroName) {
-    case "firstHero":
-      switch (direction)
-      {
-        case "up":
-          heroPosition.classList.add("firstHero_back-side");
-          break;
-        case "down":
-          heroPosition.classList.remove("firstHero_back-side");
-          break;
-        case "left":
-          heroPosition.classList.add("firstHero_left-side");
-          break;
-        case "right":
-          heroPosition.classList.add("firstHero_right-side");
-          break;
-        default:
-          break;
-      }
-      break;
-    
-    case "secondHero":
-      switch (direction)
-      {
-        case "up":
-          heroPosition.classList.add("secondHero_back-side");
-          break;
-        case "down":
-          heroPosition.classList.remove("secondHero_back-side");
-          break;
-        case "left":
-          heroPosition.classList.add("secondHero_left-side");
-          break;
-        case "right":
-          heroPosition.classList.add("secondHero_right-side");
-          break;
-        default:
-          break;
-      }
+      heroPosition.classList.add("firstHero_right-side");
       break;
     default:
       break;
   }
 }
 
+/**
+ * Show second hero turned in the correct direction
+ * @memberof MovementFunctions
+ * @param {string} direction - last movement side
+ */
+function rotateSecondHero(direction, heroPosition)
+{
+  switch (direction)
+  {
+    case "up":
+      heroPosition.classList.add("secondHero_back-side");
+      break;
+    case "down":
+      heroPosition.classList.remove("secondHero_back-side");
+      break;
+    case "left":
+      heroPosition.classList.add("secondHero_left-side");
+      break;
+    case "right":
+      heroPosition.classList.add("secondHero_right-side");
+      break;
+    default:
+      break;
+  }
+}
+
+
+/**
+ * Replace hero picture
+ * @memberof MovementFunctions
+ * @param {array} heroCoordinates - two hero coordinates 
+ * @param {string} heroName - hero name
+ * @param {string} direction -hero direction
+ */
+function moveHero(heroCoordinates, heroName, direction)
+{
+  let currentHeroStep = findField(heroCoordinates[0], heroCoordinates[1]);
+  clearMovement(currentHeroStep);
+
+  heroSpeed = checkBorder(heroCoordinates, direction);
+  let heroPosition;
+
+
+  clearAttackZone();
+  switch (direction)
+  {
+    case "up":
+      heroCoordinates[1] -= heroSpeed;
+      break;
+    case "down":
+      heroCoordinates[1] += heroSpeed;
+      break;
+    case "left":
+      heroCoordinates[0] -= heroSpeed;
+      break;
+    case "right":
+      heroCoordinates[0] += heroSpeed;
+      break;
+    default:
+      break;
+  }
+  heroPosition = findField(heroCoordinates[0], heroCoordinates[1]);
+  heroPosition.classList.add(heroName);
+  createNewAttackZone();
+
+  switch (heroName)
+  {
+    case "firstHero":
+      rotateFirstHero(direction, heroPosition);
+      break;
+    case "secondHero":
+      rotateSecondHero(direction, heroPosition);
+      break;
+    default:
+      break;
+  }
+}
+
+/**
+ * Show steps quantity on the screen
+ */
 function drawRemainingSteps()
 {
   let remainingSteps = config.step - heroStepCounter;
@@ -109,12 +165,11 @@ function drawRemainingSteps()
 }
 
 let heroStepCounter = 0;
-let heroDirection;
 let spawnEnemyTurn = 0;
 
-document.addEventListener("keydown", function(event)
+function isLastStep()
 {
-  if (heroStepCounter + 1 > config.step)
+  if (heroStepCounter + 1 >= config.step + 1)
   {
     enemiesList.forEach(enemy =>
     {
@@ -131,63 +186,62 @@ document.addEventListener("keydown", function(event)
       spawnEnemyTurn = 0;
     }
   }
-  if (heroStepCounter > config.step)
-  {    
-    heroDirection = "none";    
+}
+
+
+let heroDirection = "none";
+/**
+ * @function movementButtonController
+ * @memberof MovementFunctions
+ * @description
+ */
+document.addEventListener("keydown", function(event)
+{
+  isLastStep();
+  if (event.code == "KeyW")
+  {
+    heroDirection = "up";
+    moveHero(firstHeroCoordinates, "firstHero", heroDirection);
   }
-  else
-  {    
-    if (event.code == "KeyW")
-    {
-      heroDirection = "up";
-      moveHero(firstHeroCoordinates, "firstHero", heroDirection);
-      heroStepCounter++;      
-    }
-    if (event.code == "KeyS")
-    {
-      heroDirection = "down";
-      moveHero(firstHeroCoordinates, "firstHero", heroDirection);
-      heroStepCounter++;
-    }
-    if (event.code == "KeyA")
-    {
-      heroDirection = "left";
-      moveHero(firstHeroCoordinates, "firstHero", heroDirection);
-      heroStepCounter++;
-    }
-    if (event.code == "KeyD")
-    {
-      heroDirection = "right";
-      moveHero(firstHeroCoordinates, "firstHero", heroDirection);
-      heroStepCounter++;
-    }
-    if (event.keyCode == 38)
-    {
-      heroDirection = "up";
-      moveHero(secondHeroCoordinates, "secondHero", heroDirection);
-      heroStepCounter++;
-    }
-    if (event.keyCode == 40)
-    {
-      heroDirection = "down";
-      moveHero(secondHeroCoordinates, "secondHero", heroDirection);
-      heroStepCounter++;
-    }
-    if (event.keyCode == 37)
-    {
-      heroDirection = "left";
-      moveHero(secondHeroCoordinates, "secondHero", heroDirection);
-      heroStepCounter++;
-    }
-    if (event.keyCode == 39)
-    {
-      heroDirection = "right";
-      moveHero(secondHeroCoordinates, "secondHero", heroDirection);
-      heroStepCounter++;
-    }
-    drawRemainingSteps();
+  if (event.code == "KeyS")
+  {
+    heroDirection = "down";
+    moveHero(firstHeroCoordinates, "firstHero", heroDirection);
   }
+  if (event.code == "KeyA")
+  {
+    heroDirection = "left";
+    moveHero(firstHeroCoordinates, "firstHero", heroDirection);
+  }
+  if (event.code == "KeyD")
+  {
+    heroDirection = "right";
+    moveHero(firstHeroCoordinates, "firstHero", heroDirection);
+  }
+
+  if (event.keyCode == 38)
+  {
+    heroDirection = "up";
+    moveHero(secondHeroCoordinates, "secondHero", heroDirection);
+  }
+  if (event.keyCode == 40)
+  {
+    heroDirection = "down";
+    moveHero(secondHeroCoordinates, "secondHero", heroDirection);
+  }
+  if (event.keyCode == 37)
+  {
+    heroDirection = "left";
+    moveHero(secondHeroCoordinates, "secondHero", heroDirection);
+  }
+  if (event.keyCode == 39)
+  {
+    heroDirection = "right";
+    moveHero(secondHeroCoordinates, "secondHero", heroDirection);
+  }
+  heroStepCounter++;
+  drawRemainingSteps();
 });
 
-moveHero(firstHeroCoordinates, "firstHero");
+moveHero(firstHeroCoordinates, "firstHero"); //first turn generate
 moveHero(secondHeroCoordinates, "secondHero");
