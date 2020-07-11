f3a8545c9015c16437d0d16c7abf46b00da1db92
offllane <offllane@mail.ru>
2020-07-11 22:10:29 +0300

    set authentication check

--- game/views.py
+++ game/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render, redirect
 from game.forms import UserRegistrationForm, LoginForm
 from game.models import  CustomUser
 
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url="/login")
 def index(request):
     players = CustomUser.objects.all()
     return render(request,'game/index.html', {'players':players})
