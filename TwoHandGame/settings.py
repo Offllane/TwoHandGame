519dca24f1b14e9012729ca77b558378884f1cfe
MyCoPIIIiK <vital77717@gmail.com>
2020-07-19 13:16:45 +0300

    updates for Heroku deployment

--- TwoHandGame/settings.py
+++ TwoHandGame/settings.py
@@ -153,5 +153,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 import dj_database_url
-db_from_env = dj_database_url
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
