8da3eaf705431acba5baf5c0fd1fad8564ac86af
MyCoPIIIiK <vital77717@gmail.com>
2020-07-19 13:56:58 +0300

    updates for Heroku deployment

--- TwoHandGame/settings.py
+++ TwoHandGame/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -44,14 +46,15 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'game.CustomUser'
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'TwoHandGame.urls'
@@ -130,16 +133,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 # DJANGO_LOG_LEVEL = DEBUG
 # DataFlair Static Files Settings
 
 STATIC_URL = '/static/'
 STATIC_DIR = (os.path.join(BASE_DIR,'static'))
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
 
 # STATIC_ROOT = os.path.join('home/forbz/PycharmProjects/TwoHandGame', 'static')
 # STATICFILES_FINDER = (
@@ -148,10 +153,12 @@ STATICFILES_DIRS = [
 #     'django.contrib.staticfiles.finders.DefaultStorageFinder',
 # )
 
+
+
 #DataFlair #User_Uploaded_Files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
