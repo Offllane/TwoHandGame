e10549f025f938dfd99792d91bf7e33933979a9d
MyCoPIIIiK <vital77717@gmail.com>
2020-07-19 12:17:43 +0300

    updates for Heroku deployment

--- TwoHandGame/settings.py
+++ TwoHandGame/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'TwoHandGame.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'game',
+        'USER': 'forbz',
+        'PASSWORD': '7751876',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
