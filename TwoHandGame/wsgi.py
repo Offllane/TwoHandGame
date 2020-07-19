2af0f2acc740328c819f868eb2738f63f20aa91c
MyCoPIIIiK <vital77717@gmail.com>
2020-07-19 13:11:22 +0300

    updates for Heroku deployment

--- TwoHandGame/wsgi.py
+++ TwoHandGame/wsgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from whitenoise import WhiteNoise
+# from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwoHandGame.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+# application = WhiteNoise(application)
