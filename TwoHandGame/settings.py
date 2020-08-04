b25e2ce7aa01a7a663dc126bbc9d3cb00148d7b4
offllane <offllane@mail.ru>
2020-08-04 18:46:24 +0300

    try to fix issue

--- TwoHandGame/settings.py
+++ TwoHandGame/settings.py
@@ -136,11 +136,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = (os.path.join(BASE_DIR, 'static'))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 # STATIC_ROOT = os.path.join('home/forbz/PycharmProjects/TwoHandGame', 'static')
 # STATICFILES_FINDER = (
